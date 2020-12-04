53f93164301ada814bd784997461c12d9e7561bb
Matthew Wei <matthewwei35@gmail.com>
2020-12-03 23:22:18 -0800

    [add] Title component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
+import Title from './Title';
 
 function App() {
   return (
     <div className="App">
-      
+      <Title />
     </div>
   );
 }
---
+++ src/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Title() {
+  return (
+    <div>
+      <h1>Boba Shop Finder</h1>
+    </div>
+  )
+}
+
+export default Title;
